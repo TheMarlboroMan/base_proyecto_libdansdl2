716652020b342ab384459c9b67d4421580f96cee
Dani <marlborometal@gmail.com>
2016-01-18 17:41:13 +0100

    Actualiza clases de input.
    Repara el bug mediante el cual no es posible asignar dos
    teclas al mismo input.

--- class/framework/derivada/input.cpp
+++ class/framework/derivada/input.cpp
@@ -3,15 +3,15 @@
 void Input::configurar()
 {
 	//El input I_ESCAPE es la tecla SDLK_ESCAPE. Simple.
-	mapa_teclado.insert(std::make_pair(I_ESCAPE, SDL_SCANCODE_ESCAPE));
-	mapa_teclado.insert(std::make_pair(I_ESPACIO, SDL_SCANCODE_SPACE));
-	mapa_teclado.insert(std::make_pair(I_ENTER, SDL_SCANCODE_RETURN));
+	configurar_teclado(I_ESCAPE, SDL_SCANCODE_ESCAPE);
+	configurar_teclado(I_ESPACIO, SDL_SCANCODE_SPACE);
+	configurar_teclado(I_ENTER, SDL_SCANCODE_RETURN);
 
-	mapa_teclado.insert(std::make_pair(I_IZQUIERDA, SDL_SCANCODE_LEFT));
-	mapa_teclado.insert(std::make_pair(I_DERECHA, SDL_SCANCODE_RIGHT));
-	mapa_teclado.insert(std::make_pair(I_ARRIBA, SDL_SCANCODE_UP));
-	mapa_teclado.insert(std::make_pair(I_ABAJO, SDL_SCANCODE_DOWN));
+	configurar_teclado(I_IZQUIERDA, SDL_SCANCODE_LEFT);
+	configurar_teclado(I_DERECHA, SDL_SCANCODE_RIGHT);
+	configurar_teclado(I_ARRIBA, SDL_SCANCODE_UP);
+	configurar_teclado(I_ABAJO, SDL_SCANCODE_DOWN);
 
-	mapa_raton.insert(std::make_pair(I_CLICK_I, SDL_BUTTON_LEFT));
-	mapa_raton.insert(std::make_pair(I_CLICK_I, SDL_BUTTON_RIGHT));
+	configurar_raton(I_CLICK_I, SDL_BUTTON_LEFT);
+	configurar_raton(I_CLICK_I, SDL_BUTTON_RIGHT);
 }
